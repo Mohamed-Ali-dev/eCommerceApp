ded906bdd2ace67a35bcb209296819ae3416ff7e
mali <moohamedali571@gmail.com>
2025-02-19 21:35:55 +0200

    Add Product Controller and endpoints

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -7,7 +7,7 @@ public class Product : BaseEntity
 {
 [MaxLength(50)]
 public required string Name { get; set; }
-[MaxLength(250)]
+[MaxLength(500)]
 public required string Description { get; set; }
 public decimal Price { get; set; }
 public required string PictureUrl { get; set; }
