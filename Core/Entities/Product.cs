fcfab03a416b70a395051fdd3d8177ba024824c9
mali <moohamedali571@gmail.com>
2025-02-19 14:47:42 +0200

    Add Product Entity

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+[MaxLength(50)]
+public required string Name { get; set; }
+[MaxLength(250)]
+public required string Description { get; set; }
+public decimal Price { get; set; }
+public required string PictureUrl { get; set; }
+[MaxLength(50)]
+public required string Type { get; set; }
+[MaxLength(50)]
+public required string Brand { get; set; }
+public int Quantity { get; set; }
+
+}
